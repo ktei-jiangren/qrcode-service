d79033d5a258598316e656d861603fd6b7d03a4c
rui.cheng <ktei2008@gmail.com>
2018-08-23 14:01:54 +1000

    initial commit

---
+++ src/main/java/com/bentoclan/lnf/qrcodesvc/Application.java
@@ -0,0 +1,93 @@
+package com.bentoclan.lnf.qrcodesvc;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+@SpringBootApplication
+@Import({ PetsController.class })
+public class Application extends SpringBootServletInitializer {
+
+    private static final String QR_CODE_IMAGE_PATH = "D:\\temp\\MyQRCode.png";
+
+    /*
+     * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
+     */
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+    /*
+     * optimization - avoids creating default exception resolvers; not required as the serverless container handles
+     * all exceptions
+     *
+     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
+     * an expensive ObjectMapper instance.
+     */
+    @Bean
+    public HandlerExceptionResolver handlerExceptionResolver() {
+        return new HandlerExceptionResolver() {
+
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                return null;
+            }
+        };
+    }
+
+    public static void main(String[] args) throws IOException, WriterException, NotFoundException {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException, NotFoundException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+
+        QRCodeReader qrCodeReader = new QRCodeReader();
+
+        BufferedImage bufferedImage = ImageIO.read(new File(QR_CODE_IMAGE_PATH));
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        Result result = new MultiFormatReader().decode(bitmap);
+        System.out.println(result.getText());
+    }
+}
---
+++ src/main/java/com/bentoclan/lnf/qrcodesvc/PetsController.java
@@ -0,0 +1,23 @@
+package com.bentoclan.lnf.qrcodesvc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.UUID;
+
+@RestController
+@EnableWebMvc
+public class PetsController {
+
+    @RequestMapping(path = "/pets", method = RequestMethod.GET)
+    public Pet listPets() {
+        Pet newPet = new Pet();
+        newPet.setId(UUID.randomUUID().toString());
+        newPet.setBreed(PetData.getRandomBreed());
+        newPet.setDateOfBirth(PetData.getRandomDoB());
+        newPet.setName(PetData.getRandomName());
+        return newPet;
+    }
+}
